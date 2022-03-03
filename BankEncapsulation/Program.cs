25474a461e74354e9a841922271a08b6550e6eb9
brittany melton <brittanymelton26@gmail.com>
2022-03-03 13:26:27 -0500

    added class exercise instruction comments

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,52 +6,35 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
+            #region Bank Encapsulation Exercise Instructions
+            /*Create a new Console Application it BankEncapsulation
+             * Create a new public class named BankAccount
+             * In your BankAccount class, create the following:
+             * A private field of type double named balance with a value of 0
+             * Define a method named Deposit that will accept a double and store that value in the balance field
+             * Define a method named GetBalance that will return the amount stored in the balance field 
+             * In the main method of your application, create a new instance of the BankAccount class.  
+             * Allow the user of the application to Deposit money and retrieve their balance through the console.*/
+            #endregion
+
+            Console.WriteLine("Welcome to Your Bank!");
 
             BankAccount yourAccount = new BankAccount();
 
-            Console.WriteLine("Enter your deposit amount: \n");
-
-            yourAccount.Deposit(double.Parse(Console.ReadLine()));
-            Console.Clear();
-            Console.WriteLine("Your new account balance is: \n");
-            Console.WriteLine(yourAccount.GetBalance());
-            
-
-
-
-
+            do
+            {
+                Console.WriteLine("\nEnter your deposit amount: ");
+                Console.WriteLine("or enter \"done\" to exit\n");
+                string str = Console.ReadLine();
+                if (str == "done")
+                    break;
+                int x = Convert.ToInt32(str);
+                yourAccount.Deposit(x);
+                Console.Clear();
+                Console.WriteLine("\nYour new account balance is: \n");
+                Console.WriteLine(yourAccount.GetBalance());
+            }while(true);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //yourAccount.Deposit(double.Parse(Console.ReadLine())); Console.WriteLine("Or type \"done\" to exit.");
-        /*public static void AskIfDone()
-        {
-            string exit = Console.ReadLine();
-            string done = "done";
-            if (exit == done.ToLower())
-            Environment.Exit(0);
-
-        }*/
     }
 
 }
